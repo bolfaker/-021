a4ab5a8186aa3bd51bca7655639b32ab8772622a
bolfaker <114642598+bolfaker@users.noreply.github.com>
2023-01-12 14:02:34 +0700

    Update Решения.py

--- Второе полугодие/Решения.py
+++ Второе полугодие/Решения.py
@@ -10,3 +10,15 @@ for i in range(1,100):
     if int(chislo,2)>40:
         print (i, int(chislo,2))
         break
+#6        
+from turtle import *
+left(90)
+for i in range(7):
+forward(300)
+right(120)
+pu()
+for x in range(1,9):
+for y in range(1,10):
+goto(x*30,y*30)
+dot(5)
+done()
